b45e6c4b31338fd3ba5a1df1af54ace78b16e048
Gabriel Guimaraes <gabrielguimaraes@protonmail.com>
2020-10-16 16:42:49 -0300

    teste OCR com a imagem redimensionada...

--- OCR.py
+++ OCR.py
@@ -6,6 +6,7 @@ import imutils
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
 image = cv2.imread('TesteOCRDecimal/teste.png',0)
+
 cv2.imshow('original',image)
 image = imutils.resize(image, width=300)
 thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)[1]
@@ -16,7 +17,7 @@ close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 result = 255 - close 
 result = cv2.GaussianBlur(result, (5,5), 0)
 
-data = pytesseract.image_to_string(result, lang='eng',config='--psm 10 ')
+data = pytesseract.image_to_string(result, lang='por',config='tessedit_char_whitelist=1234567890, --psm 10 ')
 processed_data = ''.join(char for char in data if char.isnumeric() or char == '.')
 print(data)
 print(processed_data)
